419370b845d1b04724cc8d4014901de8c5429f13
ayat-liberatelabs <ayat@liberate-labs.com>
2024-08-29 05:23:21 +0600

    chore: Update layout.tsx and globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-"use client";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { resolver, theme } from "@/styles/index";
 import { Notifications } from "@mantine/notifications";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import Provider from "@/libs/provider";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/dropzone/styles.css";
@@ -11,7 +9,6 @@ import "@mantine/spotlight/styles.css";
 import "@mantine/code-highlight/styles.css";
 import "../styles/globals.css";
 
-const queryClient = new QueryClient();
 export default function RootLayout({
   children,
 }: {
@@ -22,11 +19,10 @@ export default function RootLayout({
       <head>
         <ColorSchemeScript />
       </head>
-      <body className="w-screen h-screen">
+      <body>
         <MantineProvider theme={{ ...theme }} cssVariablesResolver={resolver}>
           <Notifications position="top-center" />
-          <QueryClientProvider client={queryClient}></QueryClientProvider>
-          {children}
+          <Provider>{children}</Provider>
         </MantineProvider>
       </body>
     </html>
