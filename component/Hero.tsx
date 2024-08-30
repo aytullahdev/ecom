d6bf9a0372a67f1021621ed3d639681bca896d5f
Emdadul Islam <emdadulislam580@gmail.com>
2024-08-30 19:53:32 +0600

    style: update hero section style

--- component/Hero.tsx
+++ component/Hero.tsx
@@ -10,30 +10,49 @@ import {
 
 export default function Hero() {
   return (
-    <Box component="section" bg={"red"} h={500}>
+    <Flex component="section" h={500}>
       <BackgroundImage
         src="https://as1.ftcdn.net/v2/jpg/01/38/94/62/1000_F_138946263_EtW7xPuHRJSfyl4rU2WeWmApJFYM0B84.jpg"
         bgsz={"cover"}
         bgp={"center"}
         bgr={"no-repeat"}
         h={500}
+        w={"100%"}
+        pos={"relative"}
+
       >
         <Container
           bg={"white"}
           w={{
-            xs: "100%",
-            sm: "400px",
+            xs: "80%",
+            sm: "380px",
           }}
-          p={20}
+          px={{
+            xs: 10,
+            sm: 30,
+          }}
+          py={30}
           pos={"absolute"}
-          top={"50%"}
+          top={"25%"}
+          left={{
+            xs: "0%",
+            sm: "10%",
+          }}
+          style={{
+            borderRadius: "10px",
+          }}
+          
         >
-          <Title order={1}>Sell your clothes and earn money</Title>
-          <Button mt={20} fullWidth variant="outline">
+          <Title order={1} fz={{
+            xs: "20px",
+            sm: "28px",
+          }}
+          >Sell your clothes and earn money</Title>
+          <Button mt={20} fullWidth>
             Sell Now
           </Button>
         </Container>
       </BackgroundImage>
-    </Box>
+    </Flex>
   );
 }
